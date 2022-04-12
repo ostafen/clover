3c1edb5ed320046095bf6127d17e78ababf4cb19
Stefano <stefano.scafiti96@gmail.com>
2022-04-12 21:06:55 +0200

    Move boolToInt and compareValues methods inside util.go

--- criteria.go
+++ criteria.go
@@ -1,9 +1,7 @@
 package clover
 
 import (
-	"math/big"
 	"reflect"
-	"strings"
 )
 
 const (
@@ -69,41 +67,6 @@ func (f *field) Eq(value interface{}) *Criteria {
 			return reflect.DeepEqual(doc.Get(f.name), normValue)
 		},
 	}
-}
-
-func boolToInt(v bool) int {
-	if v {
-		return 1
-	}
-	return 0
-}
-
-func compareValues(v1 interface{}, v2 interface{}) (int, bool) {
-	v1Float, isFloat := v1.(float64)
-	if isFloat {
-		v2Float, isFloat := v2.(float64)
-		if isFloat {
-			return big.NewFloat(v1Float).Cmp(big.NewFloat(v2Float)), true
-		}
-	}
-
-	v1Str, isStr := v1.(string)
-	if isStr {
-		v2Str, isStr := v2.(string)
-		if isStr {
-			return strings.Compare(v1Str, v2Str), true
-		}
-	}
-
-	v1Bool, isBool := v1.(bool)
-	if isBool {
-		v2Bool, isBool := v2.(bool)
-		if isBool {
-			return boolToInt(v1Bool) - boolToInt(v2Bool), true
-		}
-	}
-
-	return 0, false
 }
 
 func (f *field) Gt(value interface{}) *Criteria {
