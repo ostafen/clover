87035002933ed8446ecfc8188e8a9bb11b1ad1f5
Stefano Scafiti <stefano.scafiti96@gmail.com>
2022-04-18 17:42:27 +0200

    Integrate in-memory storage engine (#26)
    add in memory store engine
    
    Co-authored-by: segfault99 <30441063+segfault99@users.noreply.github.com>

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/ostafen/clover
 go 1.13
 
 require (
-	github.com/dgraph-io/badger v1.6.2
 	github.com/dgraph-io/badger/v3 v3.2103.2
+	github.com/kr/pretty v0.2.0 // indirect
 	github.com/satori/go.uuid v1.2.0
-	github.com/stretchr/testify v1.7.0
+	github.com/stretchr/testify v1.7.1
+	golang.org/x/net v0.0.0-20211015210444-4f30a5c0130f // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 )
---
+++ mem.go
@@ -0,0 +1,229 @@
+package clover
+
+import (
+	"sort"
+	"sync"
+)
+
+type collection map[string]*Document
+
+type memEngine struct {
+	sync.RWMutex
+	collections map[string]collection
+}
+
+func newMemStorageEngine() StorageEngine {
+	return &memEngine{
+		RWMutex:     sync.RWMutex{},
+		collections: make(map[string]collection),
+	}
+}
+
+// Close implements StorageEngine
+func (*memEngine) Close() error {
+	return nil
+}
+
+// CreateCollection implements StorageEngine
+func (e *memEngine) CreateCollection(name string) error {
+	e.Lock()
+	defer e.Unlock()
+	if e.hasCollection(name) {
+		return ErrCollectionExist
+	}
+	e.collections[name] = make(collection)
+	return nil
+}
+
+// Delete implements StorageEngine
+func (e *memEngine) Delete(q *Query) error {
+	return e.replaceDocs(q, func(_ *Document) *Document {
+		return nil
+	})
+}
+
+// DeleteById implements StorageEngine
+func (e *memEngine) DeleteById(collectionName string, id string) error {
+	e.Lock()
+	defer e.Unlock()
+
+	c, ok := e.collections[collectionName]
+	if !ok {
+		return ErrCollectionNotExist
+	}
+
+	delete(c, id)
+
+	return nil
+}
+
+func (e *memEngine) hasCollection(collectionName string) bool {
+	_, ok := e.collections[collectionName]
+	return ok
+}
+
+// DropCollection implements StorageEngine
+func (e *memEngine) DropCollection(name string) error {
+	e.Lock()
+	defer e.Unlock()
+
+	if e.hasCollection(name) {
+		delete(e.collections, name)
+	} else {
+		return ErrCollectionNotExist
+	}
+	return nil
+}
+
+// FindAll implements StorageEngine
+func (e *memEngine) FindAll(q *Query) ([]*Document, error) {
+	docs := []*Document{}
+	err := e.IterateDocs(q, func(doc *Document) error {
+		if q.satisfy(doc) {
+			docs = append(docs, doc)
+		}
+		return nil
+	})
+
+	return docs, err
+}
+
+// FindById implements StorageEngine
+func (e *memEngine) FindById(collectionName string, id string) (*Document, error) {
+	e.RLock()
+	defer e.RUnlock()
+
+	c, ok := e.collections[collectionName]
+	if !ok {
+		return nil, ErrCollectionNotExist
+	}
+
+	return c[id], nil
+}
+
+// HasCollection implements StorageEngine
+func (e *memEngine) HasCollection(name string) (bool, error) {
+	e.RLock()
+	defer e.RUnlock()
+
+	_, ok := e.collections[name]
+	return ok, nil
+}
+
+// Insert implements StorageEngine
+func (e *memEngine) Insert(collection string, docs ...*Document) error {
+	e.Lock()
+	defer e.Unlock()
+
+	c, ok := e.collections[collection]
+	if !ok {
+		return ErrCollectionNotExist
+	}
+
+	for _, d := range docs {
+		c[d.ObjectId()] = d
+	}
+
+	return nil
+}
+
+func (e *memEngine) iterateDocs(q *Query, consumer docConsumer) error {
+	c, ok := e.collections[q.collection]
+	if !ok {
+		return ErrCollectionNotExist
+	}
+
+	sortDocs := len(q.sortOpts) > 0
+	allDocs := []*Document{}
+
+	for _, d := range c {
+		if q.satisfy(d) {
+			allDocs = append(allDocs, d)
+		}
+	}
+
+	if sortDocs {
+		sort.Slice(allDocs, func(i, j int) bool {
+			return compareDocuments(allDocs[i], allDocs[j], q.sortOpts) < 0
+		})
+	}
+
+	docsToSkip := q.skip
+	if len(allDocs) < q.skip {
+		docsToSkip = len(allDocs)
+	}
+	allDocs = allDocs[docsToSkip:]
+
+	if q.limit >= 0 && len(allDocs) > q.limit {
+		allDocs = allDocs[:q.limit]
+	}
+
+	for _, doc := range allDocs {
+		err := consumer(doc)
+		if err == errStopIteration {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+
+}
+
+// IterateDocs implements StorageEngine
+func (e *memEngine) IterateDocs(q *Query, consumer docConsumer) error {
+	e.RLock()
+	defer e.RUnlock()
+
+	return e.iterateDocs(q, consumer)
+}
+
+// Open implements StorageEngine
+func (e *memEngine) Open(path string) error {
+	return nil
+}
+
+// Update implements StorageEngine
+func (e *memEngine) Update(q *Query, updateMap map[string]interface{}) error {
+	return e.replaceDocs(q, func(doc *Document) *Document {
+		updateDoc := doc.Copy()
+		for updateField, updateValue := range updateMap {
+			updateDoc.Set(updateField, updateValue)
+		}
+		return updateDoc
+	})
+}
+
+func (e *memEngine) replaceDocs(q *Query, updater docUpdater) error {
+	e.Lock()
+	defer e.Unlock()
+
+	c, ok := e.collections[q.collection]
+	if !ok {
+		return ErrCollectionNotExist
+	}
+
+	docs := make([]*Document, 0)
+	e.iterateDocs(q, func(doc *Document) error {
+		if q.satisfy(doc) {
+			docs = append(docs, doc)
+		}
+		return nil
+	})
+
+	for _, doc := range docs {
+		key := doc.ObjectId()
+		if q.satisfy(doc) {
+			newDoc := updater(doc)
+			if newDoc == nil {
+				delete(c, key)
+			} else {
+				c[key].fields = newDoc.fields
+			}
+		}
+	}
+
+	return nil
+}
