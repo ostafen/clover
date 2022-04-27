ab770093ed395c74744f24d4346624e256c3abaf
edwinwalela <edwinwalela@gmail.com>
2022-04-27 12:26:22 +0300

    collections example

---
+++ examples/collections/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	c "github.com/ostafen/clover"
+)
+
+func main() {
+	db, _ := c.Open("clover-db")
+	defer db.Close()
+
+	// Check if collection already exists
+	collectionExists, _ := db.HasCollection("todos")
+
+	if !collectionExists {
+		// Create a collection named 'todos'
+		db.CreateCollection("todos")
+	}
+
+	// Delete collection
+	err := db.DropCollection("todos")
+	if err != nil {
+		log.Panicf("Failed to delete collection: %v", err)
+	}
+	fmt.Println("collection deleted")
+}
