9a963fecd7e210497b6ab7608ab3113989a1203f
Stefano <stefano.scafiti96@gmail.com>
2022-05-08 12:21:34 +0200

    Add some fields to TestStruct. Add TestNormalize2

--- encoding/encoding_test.go
+++ encoding/encoding_test.go
@@ -11,9 +11,12 @@ type TestStruct struct {
 	IntField    int  `clover:"int"`
 	UintField   uint `clover:"uint,omitempty"`
 	StringField string
+	FloatField  float32
+	BoolField   bool
 	TimeField   time.Time
 	SliceField  []int
 	MapField    map[string]interface{}
+	Data        []byte
 }
 
 func TestNormalize(t *testing.T) {
@@ -21,7 +24,11 @@ func TestNormalize(t *testing.T) {
 
 	s := &TestStruct{
 		TimeField:  date,
+		IntField:   10,
+		FloatField: 0.1,
+		BoolField:  true,
 		SliceField: []int{1, 2, 3, 4},
+		Data:       []byte("hello, clover!"),
 		MapField: map[string]interface{}{
 			"hello": "clover",
 		},
@@ -34,11 +41,22 @@ func TestNormalize(t *testing.T) {
 	m := ns.(map[string]interface{})
 	require.Nil(t, m["uint"]) // testing omitempty
 
+	require.IsType(t, m["Data"], []byte{})
+
 	s1 := &TestStruct{}
 	err = Convert(ns.(map[string]interface{}), s1)
 	require.NoError(t, err)
 
 	require.Equal(t, s, s1)
+}
+
+func TestNormalize2(t *testing.T) {
+	norm, err := Normalize(nil)
+	require.NoError(t, err)
+	require.Nil(t, norm)
+
+	_, err = Normalize(make(chan struct{}))
+	require.Error(t, err)
 }
 
 func TestEncodeDecode(t *testing.T) {
