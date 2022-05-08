49c6a3560371cfec88f6d2b4bdfc2276cff3348a
Stefano <stefano.scafiti96@gmail.com>
2022-05-08 12:11:58 +0200

    More robust test

--- encoding/encoding_test.go
+++ encoding/encoding_test.go
@@ -2,25 +2,39 @@ package encoding
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
 
+type TestStruct struct {
+	IntField    int
+	UintField   uint
+	StringField string
+	TimeField   time.Time
+	SliceField  []int
+	MapField    map[string]interface{}
+}
+
 func TestNormalize(t *testing.T) {
-	s := &struct {
-		IntField    int
-		UintField   uint
-		StringField string
-	}{}
+	date := time.Date(2020, 01, 1, 0, 0, 0, 0, time.UTC)
+
+	s := &TestStruct{
+		TimeField:  date,
+		SliceField: []int{1, 2, 3, 4},
+		MapField: map[string]interface{}{
+			"hello": "clover",
+		},
+	}
 
 	ns, err := Normalize(s)
 	require.NoError(t, err)
 
 	require.IsType(t, ns, map[string]interface{}{})
 
-	m := ns.(map[string]interface{})
+	s1 := &TestStruct{}
+	err = Convert(ns.(map[string]interface{}), s1)
+	require.NoError(t, err)
 
-	require.Equal(t, m["IntField"], int64(0))
-	require.Equal(t, m["UintField"], uint64(0))
-	require.Equal(t, m["StringField"], "")
+	require.Equal(t, s, s1)
 }
