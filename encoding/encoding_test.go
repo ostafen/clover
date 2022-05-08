851945671d010fc4e7431e9f40cff64a736678c2
Stefano <stefano.scafiti96@gmail.com>
2022-05-08 12:33:46 +0200

    Add TestNormalize3

--- encoding/encoding_test.go
+++ encoding/encoding_test.go
@@ -8,27 +8,32 @@ import (
 )
 
 type TestStruct struct {
-	IntField    int  `clover:"int"`
-	UintField   uint `clover:"uint,omitempty"`
-	StringField string
-	FloatField  float32
-	BoolField   bool
-	TimeField   time.Time
-	SliceField  []int
-	MapField    map[string]interface{}
-	Data        []byte
+	IntField    int                    `clover:"int,omitempty"`
+	UintField   uint                   `clover:"uint,omitempty"`
+	StringField string                 `clover:",omitempty"`
+	FloatField  float32                `clover:",omitempty"`
+	BoolField   bool                   `clover:",omitempty"`
+	TimeField   time.Time              `clover:",omitempty"`
+	IntPtr      *int                   `clover:",omitempty"`
+	SliceField  []int                  `clover:",omitempty"`
+	MapField    map[string]interface{} `clover:",omitempty"`
+	Data        []byte                 `clover:",omitempty"`
 }
 
 func TestNormalize(t *testing.T) {
 	date := time.Date(2020, 01, 1, 0, 0, 0, 0, time.UTC)
 
+	var x int = 100
+
 	s := &TestStruct{
-		TimeField:  date,
-		IntField:   10,
-		FloatField: 0.1,
-		BoolField:  true,
-		SliceField: []int{1, 2, 3, 4},
-		Data:       []byte("hello, clover!"),
+		TimeField:   date,
+		IntField:    10,
+		FloatField:  0.1,
+		StringField: "aString",
+		BoolField:   true,
+		IntPtr:      &x,
+		SliceField:  []int{1, 2, 3, 4},
+		Data:        []byte("hello, clover!"),
 		MapField: map[string]interface{}{
 			"hello": "clover",
 		},
@@ -38,10 +43,12 @@ func TestNormalize(t *testing.T) {
 	require.NoError(t, err)
 
 	require.IsType(t, ns, map[string]interface{}{})
-	m := ns.(map[string]interface{})
-	require.Nil(t, m["uint"]) // testing omitempty
 
+	m := ns.(map[string]interface{})
 	require.IsType(t, m["Data"], []byte{})
+
+	require.Nil(t, m["uint"]) // testing omitempty
+	require.Equal(t, m["IntPtr"], int64(100))
 
 	s1 := &TestStruct{}
 	err = Convert(ns.(map[string]interface{}), s1)
@@ -57,6 +64,38 @@ func TestNormalize2(t *testing.T) {
 
 	_, err = Normalize(make(chan struct{}))
 	require.Error(t, err)
+}
+
+func TestNormalize3(t *testing.T) {
+	date := time.Date(2020, 01, 1, 0, 0, 0, 0, time.UTC)
+
+	s := &TestStruct{
+		TimeField:   date,
+		UintField:   0,
+		IntField:    0,
+		FloatField:  0,
+		StringField: "",
+		BoolField:   false,
+		IntPtr:      nil,
+		SliceField:  []int{},
+		Data:        nil,
+		MapField:    map[string]interface{}{},
+	}
+
+	ns, err := Normalize(s)
+	require.NoError(t, err)
+
+	require.IsType(t, ns, map[string]interface{}{})
+	m := ns.(map[string]interface{})
+
+	require.Nil(t, m["int"])
+	require.Nil(t, m["uint"])
+	require.Nil(t, m["FloatField"])
+	require.Nil(t, m["BoolField"])
+	require.Nil(t, m["SliceField"])
+	require.Nil(t, m["Data"])
+	require.Nil(t, m["MapField"])
+	require.Nil(t, m["IntPtr"])
 }
 
 func TestEncodeDecode(t *testing.T) {
