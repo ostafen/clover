51815b05eecb2ac0b9829b38cd112072e87ea36c
Stefano Scafiti <stefano.scafiti96@gmail.com>
2022-07-16 19:00:20 +0200

    Implement expiration (TTL) (#72)
    * Extract method saveDocument()
    
    * Validation of document reserved fields. Add _expiresAt field
    
    * Implement document expiration
    
    * Add missing null check
    
    * Also set TTL when updating indexes
    
    * Also use indexes in TestExpiration
    
    * Fix TestExpiration() and Document.ExpiresAt()
    
    * Revert "Add document expiration (TTL) (#70)"
    
    This reverts commit 5d85dd582d8995a5540e935d3495b1dd78c527c7.

--- document.go
+++ document.go
@@ -106,13 +106,33 @@ func (doc *Document) SetAll(values map[string]interface{}) {
 
 // ExpiresAt returns the document expiration instant
 func (doc *Document) ExpiresAt() *time.Time {
-	exp, _ := doc.Get(expiresAtField).(*time.Time)
-	return exp
+	exp, ok := doc.Get(expiresAtField).(time.Time)
+	if !ok {
+		return nil
+	}
+	return &exp
 }
 
 // ExpiresAt sets document expiration
 func (doc *Document) SetExpiresAt(expiration time.Time) {
 	doc.Set(expiresAtField, expiration)
+}
+
+// TTL returns a duration representing the time to live of the document before expiration.
+// A negative duration means that the document has no expiration, while a zero value represents an already expired document.
+func (doc *Document) TTL() time.Duration {
+	expiresAt := doc.ExpiresAt()
+	if expiresAt == nil {
+		return time.Duration(-1)
+	}
+
+	now := time.Now()
+
+	if expiresAt.Before(now) { // document already expired
+		return time.Duration(0)
+	}
+
+	return time.Millisecond * time.Duration(expiresAt.Sub(now).Milliseconds())
 }
 
 // Unmarshal stores the document in the value pointed by v.
