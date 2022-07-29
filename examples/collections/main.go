d0a56a9167e1f7ac5b742c2d0ebec821fd67fb6a
Stefano <stefano.scafiti96@gmail.com>
2022-07-29 11:59:20 +0200

    Update module path in examples

--- examples/collections/main.go
+++ examples/collections/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"log"
 
-	c "github.com/ostafen/clover"
+	c "github.com/ostafen/clover/v2"
 )
 
 func main() {
--- examples/import-export/main.go
+++ examples/import-export/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"fmt"
 
-	c "github.com/ostafen/clover"
+	c "github.com/ostafen/clover/v2"
 )
 
 func main() {
