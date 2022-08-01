81f0642d540b58bc083be3c38006c7c476f51214
Stefano <stefano.scafiti96@gmail.com>
2022-08-01 21:41:29 +0200

    Add dist util

---
+++ util/dist.go
@@ -0,0 +1,30 @@
+package util
+
+import "math"
+
+func EuclideanDistance(x1, y1, x2, y2 float64) float64 {
+	dx := (x2 - x2)
+	dy := (y2 - y1)
+	return math.Sqrt(dx*dx + dy*dy)
+}
+
+func hsin(theta float64) float64 {
+	return math.Pow(math.Sin(theta/2), 2)
+}
+
+func HaversineDistance(lat1, lon1, lat2, lon2 float64) float64 {
+	// convert to radians
+	// must cast radius as float to multiply later
+	var la1, lo1, la2, lo2, r float64
+	la1 = lat1 * math.Pi / 180
+	lo1 = lon1 * math.Pi / 180
+	la2 = lat2 * math.Pi / 180
+	lo2 = lon2 * math.Pi / 180
+
+	r = 6378100 // Earth radius in METERS
+
+	// calculate
+	h := hsin(la2-la1) + math.Cos(la1)*math.Cos(la2)*hsin(lo2-lo1)
+
+	return 2 * r * math.Asin(math.Sqrt(h))
+}
