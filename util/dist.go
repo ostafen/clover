b9c842332a34f311b7a4f6c7e3e7c43cddda5b17
Stefano <stefano.scafiti96@gmail.com>
2022-08-02 12:19:49 +0200

    Add constant EarthRadiusMeters

--- util/dist.go
+++ util/dist.go
@@ -12,19 +12,19 @@ func hsin(theta float64) float64 {
 	return math.Pow(math.Sin(theta/2), 2)
 }
 
+const EarthRadiusMeters = 6378100
+
 func HaversineDistance(lat1, lon1, lat2, lon2 float64) float64 {
 	// convert to radians
 	// must cast radius as float to multiply later
-	var la1, lo1, la2, lo2, r float64
+	var la1, lo1, la2, lo2 float64
 	la1 = lat1 * math.Pi / 180
 	lo1 = lon1 * math.Pi / 180
 	la2 = lat2 * math.Pi / 180
 	lo2 = lon2 * math.Pi / 180
 
-	r = 6378100 // Earth radius in METERS
-
 	// calculate
 	h := hsin(la2-la1) + math.Cos(la1)*math.Cos(la2)*hsin(lo2-lo1)
 
-	return 2 * r * math.Asin(math.Sqrt(h))
+	return 2 * EarthRadiusMeters * math.Asin(math.Sqrt(h))
 }
