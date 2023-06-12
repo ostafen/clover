24762cfdb35d0b60d7dd302ce1c184680845ce1c
shane.xb.qian <shane.qian@foxmail.com>
2023-06-12 22:29:38 +0800

    4. modify example for that

--- examples/custom-store/main.go
+++ examples/custom-store/main.go
@@ -3,18 +3,11 @@ package main
 import (
 	"log"
 
-	"github.com/dgraph-io/badger/v3"
 	c "github.com/ostafen/clover/v2"
-	badgerstore "github.com/ostafen/clover/v2/store/badger"
 )
 
 func main() {
-	store, err := badgerstore.Open(badger.DefaultOptions("").WithInMemory(true))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	db, err := c.OpenWithStore(store)
+	db, err := c.Open("clover-db", map[string]interface{}{"dbStore": "badger"})
 	if err != nil {
 		log.Fatal(err)
 	}
